79f1891bdc8f2b61a84bad9e496c454b2d11ded0
Jonathas Rosa <jonathasrosa85@gmail.com>
2023-05-06 17:13:13 -0300

    Create Havite-Chat

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ var app = express();
 var http = require("http").createServer(app);
 var io = require("socket.io")(http);
 
+io.on("connection", (socket) => {
+    socket.on("disconnect", () => {
+        console.log("X desconectou: " + socket.id)
+    })
+});
+
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
